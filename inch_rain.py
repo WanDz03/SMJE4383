07f549bfc1caa4c9ad7765bc5556c189739b0605
WanDz03 <dzafirulkimi@gmail.com>
2022-11-01 15:43:43 +0800

    push

---
+++ inch_rain.py
@@ -0,0 +1,13 @@
+#Input how many rainfall
+inches_str = input("How many inches of rain gave fallen?: ")
+inches_int = int(inches_str)
+volume = (inches_int/12)*43560
+gallons = volume * 7.48051945
+print(inches_int," in rain on 1 acre is", gallons, "gallons")
+
+#float
+inches_str = input("How many inches of rain gave fallen?: ")
+inches_float = float(inches_str)
+volume = (inches_float/12)*43560
+gallons = volume * 7.48051945
+print(inches_float," in rain on 1 acre is", gallons, "gallons")
