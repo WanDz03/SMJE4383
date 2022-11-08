cb762bc93142dbccdba9e488ca411f62e3a04ad0
WanDz03 <dzafirulkimi@gmail.com>
2022-11-08 14:25:32 +0800

    0811

---
+++ if.py
@@ -0,0 +1,6 @@
+first_int = int(input('Please enter the 1st integer: '))
+second_int = int(input('Please enter the 2nd integer: '))
+if first_int > second_int:
+	print("First integer is bigger")
+else:
+	print("Second Integer is bigger")
