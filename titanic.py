4a5860f18633dda164858b3008ca7d9e6f168b81
WanDz03 <dzafirulkimi@gmail.com>
2022-11-22 15:53:25 +0800

    2211

--- titanic.py
+++ titanic.py
@@ -4,4 +4,10 @@ file = open("TitanicSurvival.csv")
 data = list(csv.reader(file, delimiter=","))
 file.close()
 
-print(data[-10:-1])
+#for each_ele in data:
+	#last_ele = each_ele[-1]
+	#first_ele = each_ele[0]
+	#print(last_ele)
+data[-1].append('Good Person')
+
+print(data[-1])
